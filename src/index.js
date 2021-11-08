215a79ec2b899c1c0c6e96cf1e4fb9fa632f9946
dslyvka <daniilslyvka@gmail.com>
2021-11-09 02:13:27 +0300

    intro

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
 import './sass/main.scss';
+
+import anime from '../node_modules/animejs/lib/anime.es.js';
+
+var ml4 = {};
+ml4.opacityIn = [0, 1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: '.ml4 .letters-1',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn,
+  })
+  .add({
+    targets: '.ml4 .letters-1',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: 'easeInExpo',
+    delay: ml4.delay,
+  })
+  .add({
+    targets: '.ml4 .letters-2',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn,
+  })
+  .add({
+    targets: '.ml4 .letters-2',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: 'easeInExpo',
+    delay: ml4.delay,
+  })
+  .add({
+    targets: '.ml4 .letters-3',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn,
+  })
+  .add({
+    targets: '.ml4 .letters-3',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: 'easeInExpo',
+    delay: ml4.delay,
+  })
+  .add({
+    targets: '.ml4',
+    opacity: 0,
+    duration: 500,
+    delay: 500,
+  });
+
+let screenH = window.screen.height;
+
+setTimeout(() => {
+  anime({
+    targets: '.container',
+    translateY: -screenH,
+    duration: 2800,
+  });
+}, 5500);
